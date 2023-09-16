9dadd9ab135a50dc7242021eaed2de4b36886c47
abmantis <amfcalt@gmail.com>
2023-09-16 16:32:03 +0100

    Don't lazy load LSP
    Mason uses the buffer attach event to do its thing, so this would
    require always reloading the buffer once per session for LSP to work.

--- lua/abmantis/plugins/lsp.lua
+++ lua/abmantis/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    event="BufRead",
     cond = not vim.g.vscode,
     dependencies = {
         -- Automatically install LSPs to stdpath for neovim
