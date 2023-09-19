53fb5cd846135b54713a4bcdcb51255917ee24dc
Abilio Costa <abilio.costa@criticaltechworks.com>
2023-09-19 15:53:01 +0100

    Add <leader>pv to vscode

--- lua/abmantis/vscode.lua
+++ lua/abmantis/vscode.lua
@@ -4,18 +4,22 @@ end
 
 local vsc = require("vscode-neovim")
 
+vim.keymap.set("n", "<leader>pv", function()
+    vsc.notify('workbench.files.action.showActiveFileInExplorer', {})
+end)
+
 vim.keymap.set("n", "<leader>pf", function()
     vsc.notify('workbench.action.quickOpen', {})
 end)
 
 vim.keymap.set("n", "<leader>ps", function()
-    vsc.notify('workbench.action.findInFiles', {query = vim.fn.expand("<cword>"), replace = ''})
+    vsc.notify('workbench.action.findInFiles', { query = vim.fn.expand("<cword>"), replace = '' })
 end)
 
 vim.keymap.set("x", "<leader>ps", function()
     vim.api.nvim_feedkeys('"py', 'n', false)
-    vim.defer_fn(function ()
-        vsc.notify('workbench.action.findInFiles', {query = vim.fn.getreg('p'), replace = ''})
+    vim.defer_fn(function()
+        vsc.notify('workbench.action.findInFiles', { query = vim.fn.getreg('p'), replace = '' })
     end, 1)
 end)
 
