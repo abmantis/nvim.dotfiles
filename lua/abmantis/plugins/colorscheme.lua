0bcbf00e3018d7027a340774348d251fd6daca75
abmantis <amfcalt@gmail.com>
2023-09-24 23:35:10 +0100

    Add catppuccin

--- lua/abmantis/plugins/colorscheme.lua
+++ lua/abmantis/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-return {
+return { {
     'rose-pine/neovim',
     name = 'rose-pine',
     cond = not vim.g.vscode,
@@ -6,4 +6,6 @@ return {
     config = function()
         vim.cmd.colorscheme("rose-pine")
     end,
+},
+    { "catppuccin/nvim", name = "catppuccin"}
 }
