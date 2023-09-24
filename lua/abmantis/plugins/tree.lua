845656808e57a30836a79e23b3d60bfa06ac9d01
abmantis <amfcalt@gmail.com>
2023-09-24 23:30:24 +0100

    Add custom lua pickers and tree plugin

---
+++ lua/abmantis/plugins/tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+    local my_utils = require("abmantis.utils")
+    my_utils.setkeymap("n", "<leader>pv", vim.cmd.NvimTreeFindFile, "Reveal file in file tree")
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
