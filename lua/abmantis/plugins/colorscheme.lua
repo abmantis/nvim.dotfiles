7cb3b1b463673201a74665860cfa498f2c7419db
abmantis <amfcalt@gmail.com>
2023-09-26 00:57:05 +0100

    Switch themes

--- lua/abmantis/plugins/colorscheme.lua
+++ lua/abmantis/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
-return { {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    cond = not vim.g.vscode,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("rose-pine")
-    end,
-},
-    { "catppuccin/nvim", name = "catppuccin"}
+return {
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        cond = not vim.g.vscode,
+        priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme("rose-pine")
+        -- end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        cond = not vim.g.vscode,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+    }
 }
