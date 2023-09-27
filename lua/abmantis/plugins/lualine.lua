2d9bc4668183b4a9b9f8a15c1185580431cd8381
abmantis <amfcalt@gmail.com>
2023-09-27 23:13:12 +0100

    Add lualine

---
+++ lua/abmantis/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require('lualine').setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', newfile_status = true, path = 1, } },
+        lualine_x = { 'fileformat', { 'filetype', icon_only = true } },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    })
+  end,
+}
