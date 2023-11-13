46bc9b990ade2ed1aae00cde5e9cfff4f3c0a6e6
abmantis <amfcalt@gmail.com>
2023-11-13 22:36:17 +0000

    Add fugitive

---
+++ lua/abmantis/plugins/fugitive.lua
@@ -0,0 +1,8 @@
+return {
+    'tpope/vim-fugitive',
+    cond = not vim.g.vscode,
+    config = function()
+        local my_utils = require('abmantis.utils')
+        my_utils.setkeymap('n', '<leader>pg', ':Ggr <cword>', "Grep word under cursor")
+    end
+}
