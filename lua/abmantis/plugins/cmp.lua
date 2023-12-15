5c41d08ebce14adccd3e8b7806aff29855ed5d5b
Abilio Costa <abilio.costa@criticaltechworks.com>
2023-12-15 17:27:35 +0000

    Add cmp map to close window
    This allows seeing Copilot ghost text properly, since Esc would cancel
    both cmp and copilot.

--- lua/abmantis/plugins/cmp.lua
+++ lua/abmantis/plugins/cmp.lua
@@ -32,6 +32,7 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-u>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
+                ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm {
                     -- behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
