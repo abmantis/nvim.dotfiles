315619b31b75b9eae01d493d03c1781f9c3ccf7a
Abilio Costa <abilio.costa@criticaltechworks.com>
2024-02-01 22:23:18 +0000

    Use Edge theme

--- lua/abmantis/plugins/colorscheme.lua
+++ lua/abmantis/plugins/colorscheme.lua
@@ -9,12 +9,22 @@ return {
         -- end,
     },
     {
+        "sainnhe/edge",
+        name = "edge",
+        cond = not vim.g.vscode,
+        priority = 1000,
+        config = function()
+            vim.g.edge_style = "neon"
+            vim.cmd.colorscheme("edge")
+        end,
+    },
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         cond = not vim.g.vscode,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("catppuccin-mocha")
+            -- vim.cmd.colorscheme("catppuccin-mocha")
         end,
     }
 }
