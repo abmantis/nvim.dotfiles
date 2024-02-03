2faa0cc72a58c8c08fa7c9d4172a4530b0cf0063
abmantis <amfcalt@gmail.com>
2024-02-03 01:23:52 +0000

    Automatically cd into dir from arg

--- lua/abmantis/init.lua
+++ lua/abmantis/init.lua
@@ -1,3 +1,11 @@
+-- detect if a directory was passed as argument and automatically cd into it
+if vim.fn.argc() > 0 then
+  local arg = vim.fn.argv(0)
+  if vim.fn.isdirectory(arg) == 1 then
+    vim.cmd("cd " .. arg)
+  end
+end
+
 require("abmantis.remap")
 require("abmantis.set")
 require("abmantis.lazy")
