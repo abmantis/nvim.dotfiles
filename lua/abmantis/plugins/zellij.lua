5398369419f5ab26d70730c66871a303ab9ece2a
abmantis <amfcalt@gmail.com>
2024-06-03 23:55:27 +0100

    Fix zellij no default mappings setting

--- lua/abmantis/plugins/zellij.lua
+++ lua/abmantis/plugins/zellij.lua
@@ -2,10 +2,11 @@ return {
     'fresh2dev/zellij.vim',
     cond = not vim.g.vscode,
     event = "VeryLazy",
+    init = function()
+        vim.g.zellij_navigator_no_default_mappings = 1
+    end,
     config = function()
         local utils = require('abmantis.utils')
-
-        vim.g.zellij_navigator_no_default_mappings = 1
 
         utils.setkeymap("n", "<A-h>", ":ZellijNavigateLeft<CR>", "Move to left zellij pane")
         utils.setkeymap("n", "<A-j>", ":ZellijNavigateDown<CR>", "Move to down zellij pane")
