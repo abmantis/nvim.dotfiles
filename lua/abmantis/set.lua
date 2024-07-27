88f993473cf73d2937afd747b7d320472bd43e67
abmantis <amfcalt@gmail.com>
2024-07-27 23:29:54 +0100

    Make splits more natural

--- lua/abmantis/set.lua
+++ lua/abmantis/set.lua
@@ -7,6 +7,9 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
