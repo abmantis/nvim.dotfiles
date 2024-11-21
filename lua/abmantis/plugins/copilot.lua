f99f95378470f7c5deea45f23c6b9c15ae8115c3
abmantis <amfcalt@gmail.com>
2024-11-21 20:04:17 +0000

    Re-add copilot

--- lua/abmantis/plugins/copilot.lua
+++ lua/abmantis/plugins/copilot.lua
@@ -1,8 +1,8 @@
 return {
-    -- 'github/copilot.vim',
-    -- cond = not vim.g.vscode,
-    -- event = "VeryLazy",
-    -- config = function()
-    --     vim.g.copilot_enabled = 0
-    -- end,
+    'github/copilot.vim',
+    cond = not vim.g.vscode,
+    event = "VeryLazy",
+    config = function()
+        vim.g.copilot_enabled = 0
+    end,
 }
