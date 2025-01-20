d20dd613403b997e460f6c9fb89973596d670741
abmantis <amfcalt@gmail.com>
2025-01-20 16:30:48 +0000

    Use vscode theme

--- lua/abmantis/plugins/colorscheme.lua
+++ lua/abmantis/plugins/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
     {
+        "Mofiqul/vscode.nvim",
+        cond = not vim.g.vscode,
+        config = function()
+            vim.cmd.colorscheme("vscode")
+        end,
+    },
+    {
         "sainnhe/edge",
         name = "edge",
         cond = not vim.g.vscode,
         priority = 1000,
         config = function()
             vim.g.edge_style = "neon"
-            vim.cmd.colorscheme("edge")
         end,
+        lazy = true,
     },
     {
         'rose-pine/neovim',
