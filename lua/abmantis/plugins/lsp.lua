e7166d5a2b3748784233cd2c32bff447d5980551
abmantis <amfcalt@gmail.com>
2025-02-22 15:25:32 +0000

    Add back missing LSP keymaps

--- lua/abmantis/plugins/lsp.lua
+++ lua/abmantis/plugins/lsp.lua
@@ -13,6 +13,24 @@ return {
         { 'j-hui/fidget.nvim',       opts = {} },
     },
     config = function()
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                local my_utils = require("abmantis.utils")
+                local map = function(mode, keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
+                    my_utils.setkeymap(mode, keys, func, desc, { buffer = ev.buf })
+                end
+
+                map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename')
+                map('n', '<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+                map('n', '<leader>K', vim.lsp.buf.signature_help, 'Signature Documentation')
+            end,
+        })
         --  Add any additional override configuration in the following tables. They will be passed to
         --  the `settings` field of the server config. You must look up that documentation yourself.
         --
