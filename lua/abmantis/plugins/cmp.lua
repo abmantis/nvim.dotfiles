f01d728209ca2b1d69c43be07530faaaa1655e68
abmantis <amfcalt@gmail.com>
2025-03-26 23:32:09 +0000

    Update blink

--- lua/abmantis/plugins/cmp.lua
+++ lua/abmantis/plugins/cmp.lua
@@ -1,4 +1,3 @@
--- Autocompletion
 return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
@@ -6,20 +5,20 @@ return {
   cond = not vim.g.vscode,
 
   -- use a release tag to download pre-built binaries
-  version = 'v0.11',
+  -- version = 'v1.0.0',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- On musl libc based systems you need to add this flag
   -- build = 'RUSTFLAGS="-C target-feature=-crt-static" cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+  build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     keymap = { preset = 'enter' },
-    sources = {
-      cmdline = {}
+    cmdline = {
+      sources = {}
     },
     appearance = {
       -- sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -38,63 +37,3 @@ return {
     -- trigger = { signature_help = { enabled = true } }
   }
 }
--- return {
---     'hrsh7th/nvim-cmp',
---     cond = not vim.g.vscode,
---     event = "BufRead",
---     dependencies = {
---         -- Snippet Engine & its associated nvim-cmp source
---         'L3MON4D3/LuaSnip',
---         'saadparwaiz1/cmp_luasnip',
---
---         -- Adds LSP completion capabilities
---         'hrsh7th/cmp-nvim-lsp',
---
---         -- Adds a number of user-friendly snippets
---         'rafamadriz/friendly-snippets',
---     },
---     config = function()
---         local cmp = require 'cmp'
---         local luasnip = require 'luasnip'
---         require('luasnip.loaders.from_vscode').lazy_load()
---         luasnip.config.setup {}
---
---         cmp.setup {
---             snippet = {
---                 expand = function(args)
---                     luasnip.lsp_expand(args.body)
---                 end,
---             },
---             mapping = cmp.mapping.preset.insert {
---                 ['<C-n>'] = cmp.mapping.select_next_item(),
---                 ['<C-p>'] = cmp.mapping.select_prev_item(),
---                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---                 ['<C-u>'] = cmp.mapping.scroll_docs(4),
---                 ['<C-Space>'] = cmp.mapping.complete {},
---                 ['<C-e>'] = cmp.mapping.abort(),
---                 ['<CR>'] = cmp.mapping.confirm {
---                     -- behavior = cmp.ConfirmBehavior.Replace,
---                     select = true,
---                 },
---                 ['<Tab>'] = cmp.mapping(function(fallback)
---                     if luasnip.expand_or_locally_jumpable() then
---                         luasnip.expand_or_jump()
---                     else
---                         fallback()
---                     end
---                 end, { 'i', 's' }),
---                 ['<S-Tab>'] = cmp.mapping(function(fallback)
---                     if luasnip.locally_jumpable(-1) then
---                         luasnip.jump(-1)
---                     else
---                         fallback()
---                     end
---                 end, { 'i', 's' }),
---             },
---             sources = {
---                 { name = 'nvim_lsp' },
---                 { name = 'luasnip' },
---             }
---         }
---     end,
--- }
