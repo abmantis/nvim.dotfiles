022fa42881e6cd66ae140ac31b9e9727274ae916
abmantis <amfcalt@gmail.com>
2025-08-09 00:30:15 +0100

    Lets try tokyonight

--- lua/abmantis/plugins/colorscheme.lua
+++ lua/abmantis/plugins/colorscheme.lua
@@ -2,9 +2,10 @@ return {
     {
         "Mofiqul/vscode.nvim",
         cond = not vim.g.vscode,
-        config = function()
-            vim.cmd.colorscheme("vscode")
-        end,
+        lazy = true,
+        -- config = function()
+        --     vim.cmd.colorscheme("vscode")
+        -- end,
     },
     {
         "sainnhe/edge",
@@ -27,5 +28,21 @@ return {
         name = "catppuccin",
         cond = not vim.g.vscode,
         lazy = true,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        cond = not vim.g.vscode,
+        config = function()
+            local opts = {}
+            local styles = require("tokyonight.colors").styles
+            ---@type Palette
+            local day_colors = {
+                bg = "#ffffff",
+            }
+            styles.day = vim.tbl_extend("force", styles.day(opts) --[[@as Palette]], day_colors)
+            vim.cmd.colorscheme("tokyonight")
+        end
     }
 }
