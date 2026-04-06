1178648eaa161b33d614da65f2998a6f22b5dfcd
abmantis <amfcalt@gmail.com>
2026-04-06 16:52:32 +0100

    Minor changes after 0.12 update

--- lua/abmantis/plugins/treesitter.lua
+++ lua/abmantis/plugins/treesitter.lua
@@ -1,31 +1,5 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    cond = not vim.g.vscode,
-    event = "BufRead",
-    build = ":TSUpdate",
-    config = function()
-        require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-            ensure_installed = { "vim", "vimdoc" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = false,
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate'
 }
